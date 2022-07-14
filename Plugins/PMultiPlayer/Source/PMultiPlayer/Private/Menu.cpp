a7025d490feeb56e748341615feac62354641057
Patrick Kelly <hey@butterflydesign.app>
2022-07-14 17:19:08 -0400

    Construct the Lobby and associated Widgets.

--- Plugins/PMultiPlayer/Source/PMultiPlayer/Private/Menu.cpp
+++ Plugins/PMultiPlayer/Source/PMultiPlayer/Private/Menu.cpp
@@ -3,3 +3,22 @@
 
 #include "Menu.h"
 
+void UMenu::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	bIsFocusable = true;
+	UWorld* World = GetWorld();
+	if(World)
+	{
+		APlayerController* PlayerController = World->GetFirstPlayerController();
+		if(PlayerController)
+		{
+			FInputModeUIOnly InputModeData;
+			InputModeData.SetWidgetToFocus(this->TakeWidget());
+			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+			PlayerController->SetInputMode(InputModeData);
+			PlayerController->SetShowMouseCursor(true);
+		}
+	}
+}
